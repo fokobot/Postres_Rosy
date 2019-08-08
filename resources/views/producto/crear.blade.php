495b84772e812c8236d00c2ce619f18237f58e15
Fabian Osorio <fabiani@uninorte.edu.co>
2019-08-08 18:19:22 -0500

    Se añadieron cards a productos.

--- resources/views/producto/crear.blade.php
+++ resources/views/producto/crear.blade.php
@@ -1,48 +1,57 @@
 @extends('layouts.principal')
 
 @section('content')
-<form method="POST" action="{{route('productos.store')}}" class="col-md-6">
-  @csrf
-  <div class="row">
-  	<div class="col-md-6">
-        <label for="nombre">Nombre del producto</label>
-        <div class="form-group has-default">
-        	<input type="text" name="nombre" value="{{old('nombre')}}" placeholder="Nombre"
-        		class="form-control form-control-default @error('nombre') is-invalid @enderror" >
-        </div>
-        @error('nombre')
-					<div class="alert alert-danger">{{ $message }}</div>
-				@enderror
-  	</div>
+<div class="col-md-9">
+<div class="card">
+  <div class="card-header">
+    Nuevo Producto
   </div>
-  <div class="row">
-  	<div class="col-md-6">
-        <label for="valordetal">Valor al detal</label>
-        <div class="form-group has-default">
-        <input type="text" name="valordetal" value="{{old('valordetal')}}" placeholder="$3000"
-        	class="form-control form-control-default @error('valordetal') is-invalid @enderror">
+  <div class="card-body">
+    <form method="POST" action="{{route('productos.store')}}" >
+      @csrf
+      <div class="row">
+        <div class="col-md-6">
+          <label for="nombre">Nombre del producto</label>
+          <div class="form-group has-default">
+            <input type="text" name="nombre" value="{{old('nombre')}}" placeholder="Nombre"
+            class="form-control form-control-default @error('nombre') is-invalid @enderror" >
+          </div>
+          @error('nombre')
+          <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
         </div>
-        @error('valordetal')
-				  <div class="alert alert-danger">{{ $message }}</div>
-				@enderror
-  	</div>
-  </div>
-  <div class="row">
-  	<div class="col-md-6">
-      <label for="valormayor">Valor al por mayor</label>
-      <div class="form-group has-default">
-      <input type="text" name="valormayor" value="{{old('valormayor')}}" placeholder="$2000"
-      	class="form-control form-control-default @error('valormayor') is-invalid @enderror">
       </div>
-      @error('valormayor')
-				<div class="alert alert-danger">{{ $message }}</div>
-			@enderror
-  	</div>
+      <div class="row">
+        <div class="col-md-6">
+          <label for="valordetal">Valor al detal</label>
+          <div class="form-group has-default">
+            <input type="text" name="valordetal" value="{{old('valordetal')}}" placeholder="$3000"
+            class="form-control form-control-default @error('valordetal') is-invalid @enderror">
+          </div>
+          @error('valordetal')
+          <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <label for="valormayor">Valor al por mayor</label>
+          <div class="form-group has-default">
+            <input type="text" name="valormayor" value="{{old('valormayor')}}" placeholder="$2000"
+            class="form-control form-control-default @error('valormayor') is-invalid @enderror">
+          </div>
+          @error('valormayor')
+          <div class="alert alert-danger">{{ $message }}</div>
+          @enderror
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md-6">
+          <button type="submit" class="btn btn-block btn-success">{{ __('Registrar Producto') }}</button>
+        </div>
+      </div>
+    </form>
   </div>
-  <div class="row">
-  	<div class="col-md-6">
-  		<button type="submit" class="btn btn-block btn-success">{{ __('Registrar Producto') }}</button>
-  	</div>
-  </div>
-</form>
+</div>
+</div>
 @endsection
--- resources/views/producto/mostrar.blade.php
+++ resources/views/producto/mostrar.blade.php
@@ -1,34 +1,28 @@
 @extends('layouts.principal')
 
+{{-- C O N T E N T --}}
 @section('content')
-<table class="table table-hover">
-
-  <thead>
-
-    <th>Nombre del producto</th>
-
-    <th>Valor al detal</th>
-
-    <th>Valor al por mayor</th>
-
-  </thead>
-
-  <tbody>
-@foreach($productos as $producto)
-
-      <tr>
-
-        <td>{{$producto->nombre}} </td>
-
-        <td>{{$producto->valordetal}} </td>
-
-        <td>{{$producto->valormayor}} </td>
-
-
-      </tr>
-@endforeach
-
-  </tbody>
-
-</table>
+<div class="card mb-4">
+  <div class="card-header">
+    Lista de Productos
+  </div>
+  <div class="card-body">
+    <table class="table table-hover">
+      <thead>
+        <th>Nombre del producto</th>
+        <th>Valor al detal</th>
+        <th>Valor al por mayor</th>
+      </thead>
+      <tbody>
+        @foreach($productos as $producto)
+        <tr>
+          <td>{{$producto->nombre}}</td>
+          <td>{{$producto->valordetal}}</td>
+          <td>{{$producto->valormayor}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
 @endsection
