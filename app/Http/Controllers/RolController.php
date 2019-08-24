848c94af225123728390daecc6f1e38c05ae8519
Fabian Osorio <fabiani@uninorte.edu.co>
2019-08-24 09:51:25 -0500

    Se creó el modulo "Rol"

---
+++ app/Http/Controllers/RolController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Rol;
+use Illuminate\Http\Request;
+
+class RolController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Rol  $rol
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Rol $rol)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Rol  $rol
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Rol $rol)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Rol  $rol
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Rol $rol)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Rol  $rol
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Rol $rol)
+    {
+        //
+    }
+}
