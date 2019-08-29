87b35f0337808ef30ce44c5dc1804a0d4507e6d9
Fabian Osorio <fabiani@uninorte.edu.co>
2019-08-29 14:22:35 -0500

    Se añadió en la migración nombre y apellidos a User

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,7 +15,8 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('name');
+            $table->string('nombre', 30);
+            $table->string('apellidos', 30);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
