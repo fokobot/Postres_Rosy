dc7b628e4364dc340072abf311bfad8d2bb3f743
Luis Manuel Cárcamo Morales <luismanuelcarcamo@gmail.com>
2019-09-01 14:43:47 -0500

    Mejora de la vista de cliente index y nuevo

--- resources/views/cliente/create.blade.php
+++ resources/views/cliente/create.blade.php
@@ -1,107 +1,117 @@
 @extends('layouts.principal')
 
 @section('content')
-<form method="POST" action="{{route('clientes.store')}}" class="col-md-6">
-  @csrf
-  <div class="row">
-    <div class="col-md-6">
-      <label for="tipo_de_documento_id">Tipo de documento</label>
-      <div class="form-group has-default">
-        <select name="tipo_de_documento_id" class="form-control form-control-default @error('tipo_de_documento_id') is-invalid @enderror">
-          @foreach ($tiposdedocumentos as $tiposdedocumento)
-            <option value="{{$tiposdedocumento->id}}">{{$tiposdedocumento->abreviatura}}</option>
-          @endforeach
-        </select>
-      </div>
-      @error('tipo_de_documento_id')
-      <div class="alert alert-danger">{{ $message }}</div>
-      @enderror
-    </div>
-    <div class="col-md-6">
-        <label for="documento">N° de documento</label>
-        <div class="form-group has-default">
-        	<input type="text" name="documento" value="{{old('documento')}}" placeholder="N° de documento"
-        		class="form-control form-control-default @error('documento') is-invalid @enderror" >
-        </div>
-        @error('documento')
-					<div class="alert alert-danger">{{ $message }}</div>
-				@enderror
-  	</div>
-  </div>
-  <div class="row">
-  	<div class="col-md-6">
-        <label for="nombre">Nombre</label>
-        <div class="form-group has-default">
-        	<input type="text" name="nombre" value="{{old('nombre')}}" placeholder="Nombre"
-        		class="form-control form-control-default @error('nombre') is-invalid @enderror" >
-        </div>
-        @error('nombre')
-					<div class="alert alert-danger">{{ $message }}</div>
-				@enderror
-  	</div>
-    <div class="col-md-6">
-        <label for="apellidos">Apellidos</label>
-        <div class="form-group has-default">
-        	<input type="text" name="apellidos" value="{{old('apellidos')}}" placeholder="Apellidos"
-        		class="form-control form-control-default @error('apellidos') is-invalid @enderror" >
-        </div>
-        @error('apellidos')
-					<div class="alert alert-danger">{{ $message }}</div>
-				@enderror
-  	</div>
-  </div>
-  <div class="row">
-  	<div class="col-md-6">
-        <label for="direccion">Dirección</label>
-        <div class="form-group has-default">
-        <input type="text" name="direccion" value="{{old('direccion')}}" placeholder="Dirección"
-        	class="form-control form-control-default @error('direccion') is-invalid @enderror">
-        </div>
-        @error('direccion')
-				  <div class="alert alert-danger">{{ $message }}</div>
-				@enderror
-  	</div>
-    <div class="col-md-6">
-      <label for="ciudad">Ciudad</label>
-      <div class="form-group has-default">
-      <input type="text" name="ciudad" value="{{old('ciudad')}}" placeholder="Ciudad"
-      	class="form-control form-control-default @error('ciudad') is-invalid @enderror">
-      </div>
-      @error('ciudad')
-				<div class="alert alert-danger">{{ $message }}</div>
-			@enderror
-  	</div>
-  </div>
-  <div class="row">
-  	<div class="col-md-6">
-      <label for="telefono">Telefono</label>
-      <div class="form-group has-default">
-      <input type="text" name="telefono" value="{{old('telefono')}}" placeholder="Teléfono"
-      	class="form-control form-control-default @error('telefono') is-invalid @enderror">
-      </div>
-      @error('telefono')
-				<div class="alert alert-danger">{{ $message }}</div>
-			@enderror
-  	</div>
-    <div class="col-md-6">
-      <label for="celular">Celular</label>
-      <div class="form-group has-default">
-      <input type="text" name="celular" value="{{old('celular')}}" placeholder="Celular"
-      	class="form-control form-control-default @error('celular') is-invalid @enderror">
-      </div>
-      @error('celular')
-				<div class="alert alert-danger">{{ $message }}</div>
-			@enderror
-  	</div>
-  </div>
-  <div class="row">
-  	<div class="col-md-6">
-  		<button type="submit" class="btn btn-block btn-success">{{ __('Registrar Cliente') }}</button>
-  	</div>
-  </div>
-</form>
+<div class="col-md-9">
+	<div class="card">
+		<div class="card-header">
+			Nuevo Cliente
+		</div>
+		<div class="card-body">
+			<form method="POST" action="{{route('clientes.store')}}" class="col-md-12">
+				@csrf
+				<div class="row">
+					<div class="col-md-6">
+						<label for="tipo_de_documento_id">Tipo de documento</label>
+						<div class="form-group has-default">
+							<select name="tipo_de_documento_id" class="form-control form-control-default @error('tipo_de_documento_id') is-invalid @enderror">
+								@foreach ($tiposdedocumentos as $tiposdedocumento)
+								<option value="{{$tiposdedocumento->id}}">{{$tiposdedocumento->abreviatura}}</option>
+								@endforeach
+							</select>
+						</div>
+						@error('tipo_de_documento_id')
+						<div class="alert alert-danger">{{ $message }}</div>
+						@enderror
+					</div>
+					<div class="col-md-6">
+						<label for="documento">N° de documento</label>
+						<div class="form-group has-default">
+							<input type="text" name="documento" value="{{old('documento')}}" placeholder="N° de documento"
+							class="form-control form-control-default @error('documento') is-invalid @enderror" >
+						</div>
+						@error('documento')
+						<div class="alert alert-danger">{{ $message }}</div>
+						@enderror
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<label for="nombre">Nombre</label>
+						<div class="form-group has-default">
+							<input type="text" name="nombre" value="{{old('nombre')}}" placeholder="Nombre"
+							class="form-control form-control-default @error('nombre') is-invalid @enderror" />
+						</div>
+						@error('nombre')
+						<div class="alert alert-danger">{{ $message }}</div>
+						@enderror
+					</div>
+					<div class="col-md-6">
+						<label for="apellidos">Apellidos</label>
+						<div class="form-group has-default">
+							<input type="text" name="apellidos" value="{{old('apellidos')}}" placeholder="Apellidos"
+							class="form-control form-control-default @error('apellidos') is-invalid @enderror" >
+						</div>
+						@error('apellidos')
+						<div class="alert alert-danger">{{ $message }}</div>
+						@enderror
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<label for="direccion">Dirección</label>
+						<div class="form-group has-default">
+							<input type="text" name="direccion" value="{{old('direccion')}}" placeholder="Dirección"
+							class="form-control form-control-default @error('direccion') is-invalid @enderror">
+						</div>
+						@error('direccion')
+						<div class="alert alert-danger">{{ $message }}</div>
+						@enderror
+					</div>
+					<div class="col-md-6">
+						<label for="ciudad">Ciudad</label>
+						<div class="form-group has-default">
+							<input type="text" name="ciudad" value="{{old('ciudad')}}" placeholder="Ciudad"
+							class="form-control form-control-default @error('ciudad') is-invalid @enderror">
+						</div>
+						@error('ciudad')
+						<div class="alert alert-danger">{{ $message }}</div>
+						@enderror
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-6">
+						<div class="form-group has-default">
+							<label for="telefono">Telefono</label>
+							<input type="text" name="telefono" value="{{old('telefono')}}" placeholder="Teléfono"          class="form-control form-control-default @error('telefono') is-invalid @enderror">
+						</div>
+						@error('telefono')
+						<div class="alert alert-danger">{{ $message }}</div>
+						@enderror
+					</div>
+					<div class="col-md-6">
+						<label for="celular">Celular</label>
+						<div class="form-group has-default">
+							<input type="text" name="celular" value="{{old('celular')}}" placeholder="Celular"
+							class="form-control form-control-default @error('celular') is-invalid @enderror">
+						</div>
+						@error('celular')
+						<div class="alert alert-danger">{{ $message }}</div>
+						@enderror
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-md-12">
+						<button type="submit" class="btn btn-block btn-success">
+							{{ __('Registrar Cliente') }}
+						</button>
+					</div>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
 @endsection
 {{-- S C R I P T S --}}
 @push('scripts')
-  @include('layouts.error')
+@include('layouts.error')
 @endpush
--- resources/views/cliente/index.blade.php
+++ resources/views/cliente/index.blade.php
@@ -1,29 +1,36 @@
 @extends('layouts.principal')
 @section('content')
-<table class="table table-hover">
-  <thead>
-    <th>Tipo de Documento</th>
-    <th>Documento</th>
-    <th>Nombre</th>
-    <th>Apellidos</th>
-    <th>Dirección</th>
-    <th>Ciudad</th>
-    <th>Telefono</th>
-    <th>Celular</th>
-  </thead>
-  <tbody>
-@foreach($clientes as $cliente)
-      <tr>
-        <td>{{$cliente->tipo_de_documento->abreviatura}}</td>
-        <td>{{$cliente->documento}}</td>
-        <td>{{$cliente->nombre}}</td>
-        <td>{{$cliente->apellidos}}</td>
-        <td>{{$cliente->direccion}}</td>
-        <td>{{$cliente->ciudad}}</td>
-        <td>{{$cliente->telefono}}</td>
-        <td>{{$cliente->celular}}</td>
-      </tr>
-@endforeach
-  </tbody>
-</table>
+<div class="card mb-4">
+  <div class="card-header">
+    Lista de Clientes
+  </div>
+  <div class="card-body">
+    <table class="table table-hover">
+      <thead>
+        <th>Tipo de Documento</th>
+        <th>Documento</th>
+        <th>Nombre</th>
+        <th>Apellidos</th>
+        <th>Dirección</th>
+        <th>Ciudad</th>
+        <th>Telefono</th>
+        <th>Celular</th>
+      </thead>
+      <tbody>
+        @foreach($clientes as $cliente)
+        <tr>
+          <td>{{$cliente->tipo_de_documento->abreviatura}}</td>
+          <td>{{$cliente->documento}}</td>
+          <td>{{$cliente->nombre}}</td>
+          <td>{{$cliente->apellidos}}</td>
+          <td>{{$cliente->direccion}}</td>
+          <td>{{$cliente->ciudad}}</td>
+          <td>{{$cliente->telefono}}</td>
+          <td>{{$cliente->celular}}</td>
+        </tr>
+        @endforeach
+      </tbody>
+    </table>
+  </div>
+</div>
 @endsection
