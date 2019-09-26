364118d3f09b5acaa037dd6c62f1e44d79b6ece3
Luis Manuel Carcamo Morales <luismanuelcarcamo@gmail.com>
2019-09-26 18:24:24 -0500

    Migración parcial de FormVenta a Vue-Bootstrap

--- resources/js/components/DetalleVenta.vue
+++ resources/js/components/DetalleVenta.vue
@@ -3,26 +3,32 @@
     <label>
       <b>Lista de Productos</b>
     </label>
-    <div class="row">
+    <b-row @keydown.enter.prevent="addProducto">
       <div class="form-group col-md-6">
         <label for="producto">Producto</label>
-        <select :disabled="productos.length==0" v-model="productoactual" class="form-control">
-          <option v-for="producto in productos" :key="producto.id" :value="producto.id">
-            {{producto.nombre}}
-          </option>
-        </select>
+        <v-select id="producto"
+          :options="productos"
+          label="nombre"
+          v-model="productoactual"
+          :disabled="productos.length===0">
+        </v-select>
       </div>
       <div class="form-group col-md-4">
-        <label>Cantidad</label>
-        <input :disabled="productos.length==0" min="1" @keydown.enter.prevent="addProducto" type="number" v-model.number="unidades" class="form-control" />
+        <label>Cantidad {{productos.length}}</label>
+        <b-input :disabled="productos.length==0" min="1"  type="number" v-model.number="unidades"></b-input>
       </div>
       <div class="form-group col-md-2">
         <label style="color: white;">FIX THIS</label>
-        <button :disabled="!productoactual || productos.length==0 || productoactual <= 0" type="button" @click.prevent="addProducto" class="btn btn-block btn-sm btn-primary">
+        <b-button
+          block
+          variant="primary"
+          size="sm"
+          :disabled="!productoactual || productos.length==0 || productoactual <= 0" 
+          @click.prevent="addProducto">
           <i class="fa fa-plus"></i>
-        </button>
+        </b-button>
       </div>
-    </div>
+    </b-row>
     <div class="alert alert-danger" v-if="errores['productos']">
       {{ errores.productos[0] }}
     </div>
@@ -35,20 +41,26 @@
         <th>Opciones</th>
       </thead>
       <tbody>
-        <tr v-for="producto in escogidos" :key="producto.id">
-          <td>{{producto.nombre}} {{producto.edition}}</td>
+        <tr v-for="(producto, index) in escogidos" :key="producto.id">
+          <td>{{producto.nombre}}</td>
           <td>$ {{valorProducto(producto) | currency}}</td>
           <td>
-            <input class="form-control" type="number" min="1" v-if="edition[producto.id]" v-model="producto.cantidad" @keydown.enter.prevent="editarProducto(producto)"/>
+            <b-input
+              type="number"
+              min="1" 
+              v-model.number="producto.cantidad"
+              @keydown.enter.prevent="editarProducto(producto)"
+              v-if="producto.edition">
+            </b-input>
             <span v-else>{{producto.cantidad}}</span>
           </td>
           <td>$ {{producto.cantidad * valorProducto(producto) | currency}}</td>
           <td>
             <button class="btn btn-sm btn-primary" type="button" @click="editarProducto(producto)">
-              <i class="fa fa-edit" v-if="!edition[producto.id]"></i>
+              <i class="fa fa-edit" v-if="!producto.edition"></i>
               <i class="fa fa-save" v-else></i>
             </button>
-            <button class="btn btn-sm btn-danger" type="button" @click="eliminarProducto(producto.id)">
+            <button class="btn btn-sm btn-danger" type="button" @click="eliminarProducto(index)">
               <i class="fa fa-trash"></i>
             </button>
           </td>
@@ -72,50 +84,43 @@
     mounted () {
       axios
       .get('/api/productos/')
-      .then(response => (this.productos = response.data, this.productoactual = this.productos[0].id))
+      .then(response => {
+        this.productos = response.data;
+        this.productoactual = this.productos[0] || {};
+      });
     },
     data() { return {
       productos : [],
       escogidos: [],
       edition: [],
-      productoactual: 0,
-      unidades: 1,
-      total: 0.0
+      productoactual: {},
+      unidades: 1
     }},
     methods: {
       addProducto: function() {
-        var pactual = this.productoactual;
-        var index = this.productos.findIndex(function(el) {
-          return el.id == pactual;
+        var index = this.productos.findIndex(el =>  {
+          return el.id == this.productoactual.id;
         });
-        var item = this.productos[index];
-        item.cantidad = Math.max(1, Math.abs(this.unidades));
-        this.edition[item.id] = false;
+        this.$set(this.productoactual, 'cantidad', Math.max(1, Math.abs(this.unidades)));
+        this.$set(this.productoactual, 'edition', false);
+        // quitar el producto de la lista de escogibles
+        console.log(this.productos.length)
         this.productos.splice(index, 1);
-        this.escogidos.push(item);
-        this.total += item.cantidad * this.valorProducto(item);
-        this.productoactual = this.productos.length === 0 ? 0 : this.productos[0].id;
+        console.log(this.productos.length)
+        // añadir el producto a la lista de escogidos
+        this.escogidos.push(this.productoactual);
+        // actualizar el valor del producto actual
+        this.productoactual = this.productos.length === 0 ? {} : this.productos[0];
       },
       editarProducto: function(producto){
-        this.$set(this.edition, producto.id, !this.edition[producto.id]);
-        this.total= this.escogidos.reduce(function(sum, current, index, vector){
-          current.cantidad = Math.max(1, Math.abs(current.cantidad));
-          return sum + this.valorProducto(current) * current.cantidad;
-        }.bind(this), 0);
+        this.$set(producto, 'edition', !producto.edition);
       },
-      eliminarProducto: function(id) {
-        var index = this.escogidos.findIndex(function(el) {
-          return el.id == id;
-        });
+      eliminarProducto: function(index) {
         var item = this.escogidos[index];
         if (item){
-          this.edition[item.id] = false;
-          this.unidades = item.cantidad;
           this.escogidos.splice(index, 1);
-          this.total -= item.cantidad * this.valorProducto(item);
-          delete item['cantidad'];
           this.productos.push(item)
-          this.productoactual = item.id;
+          this.productoactual = item;
         }
       },
       valorProducto: function(producto, cantidad){
@@ -129,6 +134,12 @@
     watch: {
       escogidos: function() {
         this.$emit("updatedProductos", this.escogidos);
+      }
+    }, computed: {
+      total: function(){
+        return this.escogidos.reduce((sum, item) => {
+          return sum + this.valorProducto(item, item.cantidad) * item.cantidad;
+        }, 0);
       }
     }
   }
--- resources/js/components/FormVenta.vue
+++ resources/js/components/FormVenta.vue
@@ -1,37 +1,39 @@
 <template>
-  <div class="card">
-    <div class="card-header">
+  <b-card>
+    <template v-slot:header>
       Nueva Venta
       <router-link to="/ventas" class="btn btn-primary btn-sm float-right">
         <i class="fa fa-list"></i> Ventas
       </router-link>
-    </div>
-    <div class="card-body">
-      <form method="POST" @submit.prevent="registrarVenta" novalidate  class="needs-validation">
-        <div class="form-row">
-          <div class="col-md-12">
-            <label>Cliente</label>
-            <v-select 
-              :options="lista_clientes" 
-              v-model="venta.cliente"
-              label="nombre"
-              :reduce="cliente => cliente.id">
+    </template>
+    <b-card-text>
+      <b-form method="POST" @submit.prevent="registrarVenta" novalidate  class="needs-validation">
+        <b-form-row>
+          <b-col>
+            <b-form-group
+              label="Cliente"
+              label-for="cliente">
+              <v-select
+                id="cliente"
+                :options="lista_clientes" 
+                label="nombre"
+                v-model="venta.cliente"
+                :reduce="cliente => cliente.id">
               </v-select>
-          </div>
-          <span>¿No encuentras el cliente? <router-link :to="{name: 'nuevo-cliente', query: {next: '/ventas/new'}}">Crea un nuevo cliente</router-link>.</span>
-        </div> 
+            </b-form-group>
+            <span>¿No encuentras el cliente? <router-link :to="{name: 'nuevo-cliente', query: {next: '/ventas/new'}}">Crea un nuevo cliente</router-link>.</span>
+          </b-col>
+        </b-form-row> 
         <hr>
         <detalle-venta @updatedProductos="updateProductos" :errores="errores">
         </detalle-venta>
         <hr>
-        <div class="row">
-          <div class="col-md-12">
-            <button type="submit" class="btn btn-block btn-success">Registrar Compra</button>
-          </div>
-        </div>
-      </form>
-    </div>
-  </div>
+        <b-form-row>
+          <b-button type="submit" block variant="success">Registrar Compra</b-button>
+        </b-form-row>
+      </b-form>
+    </b-card-text>
+  </b-card>
 </template>
 
 <script>
@@ -42,11 +44,9 @@
       'detalle-venta': DetalleVenta
     },
     mounted() {
-      axios
-      .get('/api/tipos_de_documento/')
+      axios.get('/api/tipos_de_documento/')
       .then(response => (this.tipos_de_documento = response.data, this.venta.tipo_de_documento_id = response.data[0].id))
-      axios
-      .get('/api/clientes/')
+      axios.get('/api/clientes/')
       .then(res => {
         this.clientes = res.data
       }).finally(() => {
