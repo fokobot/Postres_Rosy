f6ec89b2424d9f948a79684453e9661684384e1b
Luis Manuel Cárcamo Morales <luismanuelcarcamo@gmail.com>
2019-11-04 21:21:01 -0500

    Añadido middleware a rutas de la API

--- routes/api.php
+++ routes/api.php
@@ -14,7 +14,7 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+	return $request->user();
 });
 
 Route::get('tipos_de_documento', 'API\TipoDeDocumentoController@index');
@@ -23,7 +23,7 @@ Route::get('estados_gasto', 'API\EstadoGastoController@index');
 Route::get('productos', 'API\ProductoController@index');
 Route::delete('productos/{producto}', 'API\ProductoController@destroy');
 
-Route::group(['prefix' => 'clientes'], function() {
+Route::middleware('auth:api')->prefix('clientes')->group(function () {
 	Route::get('/', 'API\ClienteController@index');
 	Route::post('/', 'API\ClienteController@store');
 	Route::get('/{cliente}', 'API\ClienteController@show');
@@ -31,7 +31,7 @@ Route::group(['prefix' => 'clientes'], function() {
 	Route::delete('/{cliente}', 'API\ClienteController@destroy');
 });
 
-Route::middleware('auth:api')->prefix('gastos')->group(function() {
+Route::middleware('auth:api')->prefix('gastos')->group(function () {
 	Route::get('/', 'API\GastoController@index');
 	Route::get('/estados', 'API\EstadoGastoController@index');
 	Route::post('/', 'API\GastoController@store');
@@ -40,7 +40,7 @@ Route::middleware('auth:api')->prefix('gastos')->group(function() {
 	Route::delete('/{gasto}', 'API\GastoController@destroy');
 });
 
-Route::middleware('auth:api')->prefix('productos')->group(function() {
+Route::middleware('auth:api')->prefix('productos')->group(function () {
 	Route::get('/', 'API\ProductoController@index');
 	Route::post('/', 'API\ProductoController@store');
 	Route::get('/{producto}', 'API\ProductoController@show');
@@ -48,7 +48,7 @@ Route::middleware('auth:api')->prefix('productos')->group(function() {
 	Route::delete('/{producto}', 'API\ProductoController@destroy');
 });
 
-Route::middleware('auth:api')->prefix('proveedores')->group(function() {
+Route::middleware('auth:api')->prefix('proveedores')->group(function () {
 	Route::get('/', 'API\ProveedorController@index');
 	Route::post('/', 'API\ProveedorController@store');
 	Route::get('/{proveedor}', 'API\ProveedorController@show');
@@ -56,7 +56,7 @@ Route::middleware('auth:api')->prefix('proveedores')->group(function() {
 	Route::delete('/{proveedor}', 'API\ProveedorController@destroy');
 });
 
-Route::group(['prefix' => 'trabajos'], function() {
+Route::middleware('auth:api')->prefix('trabajos')->group(function () {
 	Route::get('/', 'API\TrabajoController@index');
 	Route::post('/', 'API\TrabajoController@store');
 	Route::get('/{trabajo}', 'API\TrabajoController@show');
@@ -64,7 +64,7 @@ Route::group(['prefix' => 'trabajos'], function() {
 	Route::delete('/{trabajo}', 'API\TrabajoController@destroy');
 });
 
-Route::middleware('auth:api')->prefix('empleados')->group(function() {
+Route::middleware('auth:api')->prefix('empleados')->group(function () {
 	Route::get('/', 'API\EmpleadoController@index');
 	Route::post('/', 'API\EmpleadoController@store');
 	Route::get('/{empleado}', 'API\EmpleadoController@show');
@@ -72,7 +72,7 @@ Route::middleware('auth:api')->prefix('empleados')->group(function() {
 	Route::delete('/{empleado}', 'API\EmpleadoController@destroy');
 });
 
-Route::group(['prefix' => 'ventas'], function (){
+Route::middleware('auth:api')->prefix('ventas')->group(function () {
 	Route::get('/', 'API\VentaController@index');
 	Route::get('/estados', 'API\VentaController@estados');
 	Route::post('/', 'API\VentaController@store');
@@ -81,7 +81,7 @@ Route::group(['prefix' => 'ventas'], function (){
 });
 
 /******************************************************/
-Route::group(['prefix' => 'departamentos'], function() {
+Route::middleware('auth:api')->prefix('departamentos')->group(function () {
 	Route::get('/', 'API\DepartamentoController@index');
 	Route::get('/{departamento}', 'API\DepartamentoController@show');
 });
