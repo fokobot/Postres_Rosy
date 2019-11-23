abc28392d47e4d4e3f18ce35b6dcf1173c65c384
Luis Manuel Cárcamo Morales <luismanuelcarcamo@gmail.com>
2019-11-22 23:46:14 -0500

    Corregido listado de clientes

---
+++ resources/js/components/TablaClientes.vue
@@ -0,0 +1,57 @@
+<template>
+  <table class="table table-hover">
+    <thead>
+      <th>Nombre</th>
+      <th>Apellidos</th>
+      <th>Dirección</th>
+      <th>Barrio</th>
+      <th>Telefono</th>
+      <th>Fecha de Nacimiento</th>
+      <th>Opciones</th>
+    </thead>
+    <tbody v-if="!loading">
+      <tr v-for="cliente in clientes" :key="cliente.id">
+        <td>{{cliente.persona.nombre}}</td>
+        <td>{{cliente.persona.apellidos}}</td>
+        <td>{{cliente.persona.direccion}}</td>
+        <td>{{cliente.persona.barrio}}</td>
+        <td>{{cliente.persona.telefono}}</td>
+        <td>{{cliente.persona.fecha_de_nacimiento}}</td>
+        <td>
+          <router-link
+            class="btn btn-sm btn-primary"
+            :to="{name: 'ver-cliente', params:{cliente:cliente.id}}"
+          >
+            <i class="fa fa-eye"></i>
+          </router-link>
+          <button class="btn btn-sm btn-danger" href="#" @click="eliminar(index)">
+            <i class="fa fa-trash"></i>
+          </button>
+        </td>
+      </tr>
+    </tbody>
+    <FilaCargando :cols="7" v-else />
+  </table>
+</template>
+
+<script>
+import FilaCargando from './FilaCargando';
+
+export default {
+  name: 'TablaClientes',
+  components: { FilaCargando },
+  props: {
+    clientes: {
+      type: Array,
+      required: true
+		}, 
+		loading: {
+			type: Boolean,
+			default: false
+		}
+  }
+}
+</script>
+
+<style>
+</style>
--- resources/js/views/Clientes/ListaClientes.vue
+++ resources/js/views/Clientes/ListaClientes.vue
@@ -2,55 +2,23 @@
   <div class="card mb-4">
     <div class="card-header">
       Lista de Clientes
-      <router-link class="btn btn-sm btn-primary float-right" :to="{name:'lista-clientes'}">
+      <router-link class="btn btn-sm btn-primary float-right" :to="{name:'nuevo-cliente'}">
         <i class="fa fa-plus"></i> Nuevo Cliente
       </router-link>
     </div>
     <div class="card-body">
-      <table class="table table-hover">
-        <thead>
-          <th>Nombre</th>
-          <th>Apellidos</th>
-          <th>Dirección</th>
-          <th>Barrio</th>
-          <th>Telefono</th>
-          <th>Fecha de Nacimiento</th>
-          <th>Opciones</th>
-        </thead>
-        <tbody v-if="!loading">
-          <tr v-for="cliente in clientes" :key="cliente.id">
-            <td>{{cliente.persona.nombre}}</td>
-            <td>{{cliente.persona.apellidos}}</td>
-            <td>{{cliente.persona.direccion}}</td>
-            <td>{{cliente.persona.barrio}}</td>
-            <td>{{cliente.persona.telefono}}</td>
-            <td>{{cliente.persona.fecha_de_nacimiento}}</td>
-            <td>
-              <router-link
-                class="btn btn-sm btn-primary"
-                :to="{name: 'ver-cliente', params:{cliente:cliente.id}}"
-              >
-                <i class="fa fa-eye"></i>
-              </router-link>
-              <button class="btn btn-sm btn-danger" href="#" @click="eliminar(index)">
-                <i class="fa fa-trash"></i>
-              </button>
-            </td>
-          </tr>
-        </tbody>
-        <FilaCargando :cols="7" v-else />
-      </table>
+     <tabla-clientes :clientes="clientes" :loading="loading"/>
     </div>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex';
-import FilaCargando from '../../components/FilaCargando';
+import TablaClientes from '../../components/TablaClientes';
 
 export default {
   name: 'Clientes',
-  components: { FilaCargando },
+  components: { TablaClientes },
   mounted() {
     if (this.clientes.length === 0) {
       this.$store.dispatch('clientes/fetchAll');
